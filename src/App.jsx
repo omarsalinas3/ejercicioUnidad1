a36c322703ad7f3b540a8a4d53b60789ebcab2a4
omarsalinas3 <salinasomar996@gmail.com>
2026-02-09 20:12:57 -0600

    Desafío 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from './componentes/UserProfile';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <UserProfile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile() {
+  return (
+    <div >
+      <h1>Omar</h1>
+      <h2>1223100433</h2>
+      <h2>GIDS6082</h2>
+    </div>
+  );
+}
+
+export default UserProfile;
